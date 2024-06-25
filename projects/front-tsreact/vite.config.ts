8209b65dbd49d1e2598a4a3998ec4c485c50d157
Mendoza Gutiérrez, Oscar Francisco <oscarfmg@outlook.com>
2024-06-25 16:13:28 -0600

    Add type to vite.config

--- projects/front-tsreact/vite.config.ts
+++ projects/front-tsreact/vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
+export default ({ mode }: { mode: string }) => {
   process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
   return defineConfig({
     plugins: [react()],
